051aa63406c46d17158caac0501776f341e4342e
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-03-10 20:20:12 +0100

    move products grid into shop feature

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,17 +1,1 @@
-import { ProductCard } from "@/features/shop/components/ProductCard";
-import { Grid } from "@radix-ui/themes";
-
-export default function RootPage() {
-	return (
-		<Grid columns={{ lg: "4", sm: "3", xs: "2" }} gap="3">
-			<ProductCard />
-			<ProductCard />
-			<ProductCard />
-			<ProductCard />
-			<ProductCard />
-			<ProductCard />
-			<ProductCard />
-			<ProductCard />
-		</Grid>
-	);
-}
+export { Shop as default } from "@/features/shop/Shop";
---
+++ src/features/shop/Shop.tsx
@@ -0,0 +1,17 @@
+import { Grid } from "@radix-ui/themes";
+import { ProductCard } from "./components/ProductCard";
+
+export function Shop() {
+	return (
+		<Grid columns={{ lg: "4", sm: "3", xs: "2" }} gap="3">
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+		</Grid>
+	);
+}

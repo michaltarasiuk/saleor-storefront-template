90e2389221f9c2002abaa6ee001912a5488d925c
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-03-12 15:23:04 +0100

    add private envs handling

--- env_variables.ts
+++ env_variables.ts
@@ -1,8 +1,10 @@
-import { url, object, parse, string } from "valibot";
+import { raise } from "@/shared/tools/raise";
+import { url, object, parse, string, undefined_, union } from "valibot";
 import type { Output } from "valibot";
 
 const PROCESS_ENV_SCHEMA = object({
 	NEXT_PUBLIC_GRAPHQL_ORIGIN: string([url()]),
+	APP_TOKEN: union([string(), undefined_()]),
 });
 
 parse(PROCESS_ENV_SCHEMA, process.env);
@@ -12,3 +14,9 @@ declare global {
 		interface ProcessEnv extends Output<typeof PROCESS_ENV_SCHEMA> {}
 	}
 }
+
+export const $env = {
+	get appToken() {
+		return process.env.APP_TOKEN ?? raise("app token is not defined");
+	},
+};
